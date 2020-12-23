6a301dab0a669ee41bb942ca75250126b308cb42
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-12-23 09:20:14 +0600

    Trivial compression

---
+++ ch1/trivial_compress.py
@@ -0,0 +1,51 @@
+
+class CompressedGene:
+    def __init__(self, gene: str) -> None:
+        self._compress(gene)
+
+    def _compress(self, gene: str):
+        self.bit_string: int = 1
+        for nucleotide in gene.upper():
+            self.bit_string <<= 2
+            if nucleotide == "A":
+                self.bit_string |= 0b00
+            elif nucleotide == "C":
+                self.bit_string |= 0b01
+            elif nucleotide == "G":
+                self.bit_string |= 0b10
+            elif nucleotide == "T":
+                self.bit_string |= 0b11
+            else:
+                raise ValueError("Invalid Nucleotide: {}".format(nucleotide))
+
+    def decompress(self) -> str:
+        gene: str = ""
+        for i in range(0, self.bit_string.bit_length()-1, 2):
+            bits: int = self.bit_string >> i & 0b11
+            if bits == 0b00:
+                gene += "A"
+            elif bits == 0b01:
+                gene += "C"
+            elif bits == 0b10:
+                gene += "G"
+            elif bits == 0b11:
+                gene += "T"
+            else:
+                raise ValueError(f"Invalid bits: {bits}")
+        return gene[::-1]
+
+    def __str__(self):
+        return self.decompress()
+
+
+if __name__ == "__main__":
+    import sys
+    #original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA"*100
+    original: str = "ACGT"
+    print("original is {} bytes".format(sys.getsizeof(original)))
+    compressed: CompressedGene = CompressedGene(original)
+    print("compressed is {} bytes".format(
+        sys.getsizeof(compressed.bit_string)))
+    print(compressed)  # распаковка
+    print("original and decompressed are the same: {}".format(
+        original == compressed.decompress()))
