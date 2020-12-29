49c5fa851c090844dbbb9fb290cd2c02d0c9fd56
Ilya Golubkov <ilya.golubkov@gmail.com>
2020-12-29 13:38:16 +0600

    Maze path bfs and dfs algorithms

--- ch2/generic_search.py
+++ ch2/generic_search.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import typing
 
 T = typing.TypeVar('T')
@@ -42,6 +43,99 @@ def binary_contains(sequence: typing.Sequence[C], key: C) -> bool:
         else:
             return True
     return False
+
+
+class Stack(typing.Generic[T]):
+    def __init__(self) -> None:
+        self._container: typing.List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+class Node(typing.Generic[T]):
+    def __init__(self, state: T, parent: typing.Optional[Node],
+                 cost: float = 0.0, heuristic: float = 0.0) -> None:
+        self.state: T = state
+        self.parent: typing.Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+    def __lt__(self, other: Node):
+        return(self.cost+self.heuristic) < (other.cost+other.heuristic)
+
+
+def node_to_path(node: Node[T]) -> typing.List[T]:
+    path: typing.List[T] = [node.state]
+    while node.parent is not None:
+        node = node.parent
+        path.append(node.state)
+    path.reverse()
+    return path
+
+
+class Queue(typing.Generic[T]):
+    def __init__(self) -> None:
+        self._container: typing.Deque[T] = typing.Deque()
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.popleft()
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
+def dfs(initial: T, goal_test: typing.Callable[[T], bool],
+        successors: typing.Callable[[T], typing.List[T]]) -> typing.Optional[Node[T]]:
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    explored: typing.Set[T] = {initial}
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None
+
+
+def bfs(initial: T, goal_test: typing.Callable[[T], bool],
+        successors: typing.Callable[[T], typing.List[T]]) -> typing.Optional[Node[T]]:
+    frontier: Queue[Node[T]] = Queue()
+    frontier.push(Node(initial, None))
+    explored: typing.Set[T] = {initial}
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        if goal_test(current_state):
+            return current_node
+        for child in successors(current_state):
+            if child in explored:
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None
 
 
 if __name__ == "__main__":
---
+++ ch2/maze.py
@@ -0,0 +1,96 @@
+import enum
+import typing
+import random
+import math
+from generic_search import Node, dfs, bfs, node_to_path
+
+
+class Cell(str, enum.Enum):
+    EMPTY = " "
+    BLOCKED = "X"
+    START = "S"
+    GOAL = "G"
+    PATH = "*"
+
+
+class MazeLocation(typing.NamedTuple):
+    row: int
+    column: int
+
+
+class Maze:
+    def __init__(self, rows: int = 10, columns: int = 10, sparseness: float = 0.2,
+                 start: MazeLocation = MazeLocation(0, 0),
+                 goal: MazeLocation = MazeLocation(9, 9)) -> None:
+        self._rows: int = rows
+        self._columns: int = columns
+        self.start: MazeLocation = start
+        self.goal: MazeLocation = goal
+        self._grid: typing.List[typing.List[Cell]] = [
+            [Cell.EMPTY for c in range(columns)] for r in range(rows)]
+        self._randomly_fill(rows, columns, sparseness)
+        self._grid[start.row][start.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
+
+    def _randomly_fill(self, rows: int, columns: int, sparseness: float) -> None:
+        for row in range(rows):
+            for column in range(columns):
+                if random.uniform(0, 1.0) < sparseness:
+                    self._grid[row][column] = Cell.BLOCKED
+
+    def __str__(self):
+        output: str = ""
+        for row in self._grid:
+            output += "".join([c.value for c in row]) + "\n"
+        return output
+
+    def goal_test(self, ml: MazeLocation) -> bool:
+        return ml == self.goal
+
+    def successors(self, ml: MazeLocation) -> typing.List[MazeLocation]:
+        locations: typing.List[MazeLocation] = []
+        if ml.row+1 < self._rows and self._grid[ml.row+1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row+1, ml.column))
+        if ml.row-1 >= 0 and self._grid[ml.row-1][ml.column] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row-1, ml.column))
+        if ml.column+1 < self._columns and self._grid[ml.row][ml.column+1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column+1))
+        if ml.column-1 >= 0 and self._grid[ml.row][ml.column-1] != Cell.BLOCKED:
+            locations.append(MazeLocation(ml.row, ml.column-1))
+        return locations
+
+    def mark(self, path: typing.List[MazeLocation]):
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.PATH
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+
+    def clear(self, path: typing.List[MazeLocation]):
+        for maze_location in path:
+            self._grid[maze_location.row][maze_location.column] = Cell.EMPTY
+        self._grid[self.start.row][self.start.column] = Cell.START
+        self._grid[self.goal.row][self.goal.column] = Cell.GOAL
+
+
+if __name__ == "__main__":
+    maze: Maze = Maze()
+    print(maze)
+    solution1: typing.Optional[Node[MazeLocation]] = dfs(
+        maze.start, maze.goal_test, maze.successors)
+    if solution1 is None:
+        print("No solution found")
+    else:
+        path1: typing.List[MazeLocation] = node_to_path(solution1)
+        maze.mark(path1)
+        print(maze)
+        maze.clear(path1)
+    print('\n\n')
+    solution2: typing.Optional[Node[MazeLocation]] = bfs(
+        maze.start, maze.goal_test, maze.successors)
+    if solution2 is None:
+        print("No solution found")
+    else:
+        path2: typing.List[MazeLocation] = node_to_path(solution2)
+        maze.mark(path2)
+        print(maze)
+        maze.clear(path2)
